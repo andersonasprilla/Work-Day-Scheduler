0df50f4cbc385b67e15813b4a1f9bccaf9a7fd61
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-01-20 22:04:00 -0500

    retrieve data from localstorage

--- script.js
+++ script.js
@@ -9,10 +9,7 @@ $(document).ready(function () {
   //WHEN I view the timeblocks for that day
   //THEN each timeblock is color coded to indicate whether it is in the past, present, or future
   function colorUpdater() {
-    //Get current Hour
     var currentHour = dayjs().format('HH')
-
-    //Get all elements with class "time-block"
     var timeBlock = $(".time-block")
 
     //loop through each block and update color
@@ -30,33 +27,39 @@ $(document).ready(function () {
     }
   }
 
+  //WHEN I click the save button for that timeblock
+  //THEN the text for that event is saved in local storage
+  $(".saveBtn").on("click", function () {
 
-});
+    var parentDiv = $(this).closest(".time-block");
+    var hour = parentDiv.find(".hour").text().trim();
+    var textareaValue = parentDiv.find(".description").val().trim();
 
-//WHEN I click the save button for that timeblock
-//THEN the text for that event is saved in local storage
+    // Check if the textarea is not empty
+    if (textareaValue !== "") {
+      localStorage.setItem(hour, textareaValue);
+      savedNotification();
+    }
+  });
 
-$(".saveBtn").on("click", function () {
-
-  // Get the parent div of the clicked save button
-  var parentDiv = $(this).closest(".time-block");
-  // Get the hour value from the text inside the corresponding hour div
-  var hour = parentDiv.find(".hour").text().trim();
-  // Get the value of the textarea inside the same parent div
-  var textareaValue = parentDiv.find(".description").val().trim();
-
-  // Check if the textarea is not empty
-  if (textareaValue !== "") {
-    // Add to localStorage
-    localStorage.setItem(hour, textareaValue);
-    savedNotification();
+  function savedNotification() {
+    $('#saved-to-ls').attr('class', 'd-block text-center')
+    setTimeout(() => {
+      $('#saved-to-ls').attr('class', 'd-none')
+    }, 3000);
   }
+
+  // WHEN I refresh the page
+  // THEN the saved events persist
+    $('#hour-09 .description').val(localStorage.getItem('9AM'));
+    $('#hour-10 .description').val(localStorage.getItem('10AM'));
+    $('#hour-11 .description').val(localStorage.getItem('11AM'));
+    $('#hour-12 .description').val(localStorage.getItem('12AM'));
+    $('#hour-13 .description').val(localStorage.getItem('1PM'));
+    $('#hour-14 .description').val(localStorage.getItem('2PM'));
+    $('#hour-15 .description').val(localStorage.getItem('3PM'));
+    $('#hour-16 .description').val(localStorage.getItem('4PM'));
+    $('#hour-17 .description').val(localStorage.getItem('5PM'));
 });
 
 
-function savedNotification() {
-  $('#saved-to-ls').attr('class', 'd-block text-center')
-  setTimeout(() => {
-    $('#saved-to-ls').attr('class', 'd-none')
-  }, 3000);
-}
