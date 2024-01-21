b46956cecb2ac8a99ad218171f7d3c6fadeb01ef
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-01-20 19:14:06 -0500

    save local storage

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
   colorUpdater()
 
+  //WHEN I open the planner
+  //THEN the current day is displayed at the top of the calendar
+  $("#currentDay").text(dayjs().format("dddd, MMMM D[th]"))
 
 
   //WHEN I view the timeblocks for that day
   //THEN each timeblock is color coded to indicate whether it is in the past, present, or future
   function colorUpdater() {
     //Get current Hour
-    var currentHour = "15"
+    var currentHour = dayjs().format('HH')
 
     //Get all elements with class "time-block"
     var timeBlock = $(".time-block")
@@ -27,7 +30,19 @@ $(document).ready(function () {
     }
   }
 
-  //WHEN I open the planner
-  //THEN the current day is displayed at the top of the calendar
-  $("#currentDay").text(dayjs().format("dddd, MMMM D[th]"))
+
+});
+
+//WHEN I click the save button for that timeblock
+//THEN the text for that event is saved in local storage
+
+$(".saveBtn").on("click", function () {
+  // Get the parent div of the clicked save button
+  var parentDiv = $(this).closest(".time-block");
+  // Get the hour value from the text inside the corresponding hour div
+  var hour = parentDiv.find(".hour").text().trim();
+  // Get the value of the textarea inside the same parent div
+  var textareaValue = parentDiv.find(".description").val().trim();
+  // Add to localStorage
+  localStorage.setItem(hour, textareaValue)
 });
